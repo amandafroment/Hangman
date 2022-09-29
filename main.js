26f994b116032c067f3db340e8bacebb50964f1a
Amanda <amanda.froment@hotmail.com>
2022-09-29 15:19:15 -0400

    html, css, js set up

---
+++ main.js
@@ -0,0 +1,15 @@
+// constants
+
+// state variables
+
+// cached elements
+
+// event listeners
+
+// functions
+
+// What I need to do:
+
+// the app should choose a word for the user to try and solve.
+// render that value to the page and provide the appropriate letter spaces for the word
+// Wait for a player to click a letter
