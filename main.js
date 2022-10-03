4363c072d8a27f267faebfef9d3b647c56924e22
Amanda <amanda.froment@hotmail.com>
2022-10-03 17:55:27 -0400

    html and css done, js is next

--- main.js
+++ main.js
@@ -13,3 +13,7 @@
 // the app should choose a word for the user to try and solve.
 // render that value to the page and provide the appropriate letter spaces for the word
 // Wait for a player to click a letter
+
+// have an array of words for the computer to choose from
+
+// create a function that loops through all of the letters
